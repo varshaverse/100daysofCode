026d96a0f1c1e9eb49d233cc8180cc94291fef1b
Varsha Vinod <varshaverse247@gmail.com>
2023-03-03 14:54:23 -0800

    band generator

---
+++ Day01/bandgen.py
@@ -0,0 +1,9 @@
+print("Welcome to the generator")
+
+c = input("Which city did you grow up in?\n")
+print(c)
+p = input("Whats your pet's name? \n")
+print(p)
+
+print("Your band name is " + c + " " + p)
+
