a1e68b7bdc62b79521d346fd87c7fb2adc0839d0
Varsha Vinod <varshaverse247@gmail.com>
2023-03-05 17:09:59 -0800

    more data types

--- Day02/datatype.py
+++ Day02/datatype.py
@@ -1,8 +1,9 @@
 two_digit_number = input("Type a two digit number: ")
 
+print(type(two_digit_number)) #type shows the data type the variable is in
+
 f = two_digit_number[0]
 s = two_digit_number[1]
-
 result = int(f) + int(s)
 
 print(result)
