7f7422e2c6f6ac45c78ee1c8eacef3548187814e
Varsha Vinod <varshaverse247@gmail.com>
2023-03-06 21:18:48 -0800

    height

---
+++ Day03/height.py
@@ -0,0 +1,10 @@
+
+print("Welcome to the rollercoaster")
+
+height = int(input("What is your height in cm? ")) #one equal sign indicates error
+
+if height > 120:
+    print("You cannot ride.")
+
+else:
+    print("You can ride.")
