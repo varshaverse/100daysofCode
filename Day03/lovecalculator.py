668fdbe8edb1987f1796f3e4e4437f090a7e43b9
Varsha Vinod <varshaverse247@gmail.com>
2023-03-09 21:39:47 -0800

    love calculator

--- Day03/lovecalculator.py
+++ Day03/lovecalculator.py
@@ -13,9 +13,9 @@ e = lower_case.count("e")
 
 true = t + r + u + e
 
-l = lower_case.count("t")
-o = lower_case.count("r")
-v = lower_case.count("u")
+l = lower_case.count("l")
+o = lower_case.count("o")
+v = lower_case.count("v")
 e = lower_case.count("e")
 
 love = l + o + v + e
